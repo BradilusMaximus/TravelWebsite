45ad5e163aa8a173188d0d2470115d09e0a7b2e7
BradilusMaximus <bradilusmaximus@gmail.com>
2021-11-30 01:58:20 -0500

    testing things

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import * as React from "react";
+
+const About = () => {
+  return (
+    <>
+      <h1> This is an about page.</h1>
+      <p> I see you're still here. ;)</p>
+    </>
+  );
+};
+
+export default About;
