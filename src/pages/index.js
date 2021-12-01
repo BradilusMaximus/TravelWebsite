4e4f9734c50ba776b31b33fde09aa3ca0ef819b7
BradilusMaximus <bradilusmaximus@gmail.com>
2021-12-01 04:34:17 -0500

    Added exclamation mark after world

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,7 +3,7 @@ import * as React from "react";
 const Homepage = () => {
   return (
     <>
-      <h1> Hello, World!</h1>
+      <h1> Hello, World!!</h1>
       <p> How are you, <a href="about">today?</a></p>
       
     </>
